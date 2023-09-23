76c9ab5fb2da23f0db3eec0727b33c4d5e17b7bc
lilydias24 <125706749+lilydias24@users.noreply.github.com>
2023-09-22 23:52:52 -0300

    Estilização do Cadastrar

--- cadCria.php
+++ cadCria.php
@@ -3,37 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
     <title>Cadastrar Criança</title>
 </head>
 <body>
-    <div class="container">
-        <h3> Cadastro de Criança </h3>
+    <div class="container-sm my-5 p-5 border text-center text-bg-light">
+        <p class="h4 p-3 w-50 text-danger mx-auto bg-light shadow"> Cadastro de Criança </p>
         <form action="processo.php" method="POST">
             <div class="form-group">
                 <label for="responsavel">Nome : </label>
-                <input type="text" name="nome" class="form-input" /> 
+                <input type="text" name="nome" class="form-input m-3" /> 
             </div>
             <div class="form-group">
                 <label for="responsavel">Idade : </label>
-                <input type="number" name="idade" class="form-input" /> 
+                <input type="number" name="idade" class="form-input m-3" /> 
             </div>
             <div class="form-group">
                 <label for="sexo"> Gênero : </label>
-                <input type="radio" name="sexo" class="form-input" value="boy" />Garoto
-                <input type="radio" name="sexo" class="form-input" value="girl" />Garota
-                <input type="radio" name="sexo" class="form-input" value="other" />Outro
+                <input type="radio" name="sexo" class="form-input m-3" value="boy" />Garoto
+                <input type="radio" name="sexo" class="form-input m-3" value="girl" />Garota
+                <input type="radio" name="sexo" class="form-input m-3" value="other" />Outro
             </div>
             <div class="form-group">
-                <label for="responsavel">Nome do Responsável : </label>
-                <input type="text" name="responsavel" class="form-input"> 
+                <label for="responsavel">Nome do Responsável :</label>
+                <input type="text" name="responsavel" class="form-input m-3"> 
             </div>
             <div class="form-group">
                 <label for="responsavel">Contato do Responsável : </label>
-                <input type="text" name="responsavel" class="form-input"> 
+                <input type="text" name="responsavel" class="form-input m-3"> 
             </div>
             <div class="form-group">
-                <button class="btn btn-success"><input type="submit" name="cadastrar" class="form-input" /> Cadastrar Criança</button>
+                <button class="btn btn-success m-3" type="submit" name="cadastrar" class="form-input"> Cadastrar Criança</button>
             </div>
         </form>
     </div>
