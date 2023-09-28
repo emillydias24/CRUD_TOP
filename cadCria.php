99baf67c7400c72f5bce4cd62a27fc43ecc104c5
Bianca Ramos <113118620+ramos2712@users.noreply.github.com>
2023-09-28 14:04:56 -0300

    1.1

--- cadCria.php
+++ cadCria.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
     <title>Cadastrar Criança</title>
 </head>
 <body>
-    <div class="container-sm my-5 p-5 border text-center text-bg-light">
+    <div class="container-sm my-5 p-5 border text-center text-bg-light rounded">
         <p class="h4 p-3 w-50 text-danger mx-auto bg-light shadow"> Cadastro de Criança </p>
         <form action="processo.php" method="POST">
             <div class="form-group">
@@ -31,12 +33,17 @@
             </div>
             <div class="form-group">
                 <label for="responsavel">Contato do Responsável : </label>
-                <input type="text" name="responsavel" class="form-input m-3"> 
+                <input type="text" id="contato" name="responsavel" class="form-input m-3"> 
             </div>
             <div class="form-group">
-                <button class="btn btn-success m-3" type="submit" name="cadastrar" class="form-input"> Cadastrar Criança</button>
+                <button class="btn btn-success m-3" type="submit" name="cadastrar" class="form-input">Cadastrar Criança</button>
             </div>
         </form>
     </div>
+    <script>
+        $(document).ready(function(){
+            $('#contato').mask('(00) 00000-0000');
+        });
+    </script>
 </body>
 </html>
