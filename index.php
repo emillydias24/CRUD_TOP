570e298491fcc626dfc0e29aa4540ff77e520147
Bianca Ramos <113118620+ramos2712@users.noreply.github.com>
2023-09-28 13:49:02 -0300

    index.php

--- index.php
+++ index.php
@@ -10,29 +10,30 @@
     <title>CADASTRO</title>
 </head>
 
-<body style="background-image: url('fundo.png');">
+<body style="background-image: url('fundo.png');background-size:cover; background-repeat:no-repeat; background-attachment: fixed;">
 
 
-    <div class="container">
+    <div class="container mt-5">
         <div class="text-center">         
       </div>
 
       <div class="jumbotron text-center mt-5" style="margin-bottom:0; background-color:#de7cba;">
 
-    <div class="container">
+    <div class="container mt-5">
     <div class="container p-3 my-3 border text-center">
-        <h1 class="text-light"> CADASTRE-SE</h1>
+        <h2 class="text-light"> CADASTRE-SE</h2>
                 <h3 class="text-light">Para prosseguir sua viagem, é necessário cadastrar seu E-mail e sua senha!</h3>
-                <h5 class="text-light">Lembrando que o E-mail e senha do usuário é do responsável da crinça que será posteriormente cadastrada no sistema!</h5>
+                <p class="text-light">Lembrando que o E-mail e senha do usuário é do responsável da criança que será posteriormente cadastrada no sistema.</p>
                     <form method="POST" action="cadCria.php" class="form-group">
+
                 <div class="form-group">
-                    <label  class="text-light"> E-mail </label>
-                    <input class="form-control" type="text" name="email" required="required" />
+                    <label  class="text-light"> E-mail: </label>
+                    <input class="form-control  w-50 mx-auto" type="text" name="email" required="required" />
                 </div>
 
                 <div class="form-group">
-                    <label  class="text-light"> Senha </label>
-                    <input class="form-control" type="password" name="senha" required="required" />
+                    <label  class="text-light"> Senha: </label>
+                    <input class="form-control w-50 mx-auto"  type="password" name="senha" required="required" />
                 </div>
 
                 <input class="btn btn-light" type="submit" role="button" name="logar" value="Concluir"/>
