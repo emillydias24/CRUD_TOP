6f8adc9d3bc00d65d5f0740f1d7586e960719fa5
Bianca Ramos <113118620+ramos2712@users.noreply.github.com>
2023-09-28 13:26:41 -0300

    index

--- index.php
+++ index.php
@@ -1,29 +1,3 @@
-<?php
-
-if(isset($_POST['logar'])){
-
-    include_once('conecta.php');
-
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
-
-    $sql = "SELECT * FROM usuario WHERE email='$email'";
-    $resultado = mysqli_query($conexao, $sql);  
-
-    if(mysqli_num_rows($resultado) > 0){
-        $dados = mysqli_fetch_assoc($resultado);
-        if(password_verify($senha,$dados['senha'])){
-           
-            session_start();
-            $_SESSION["idusuario"] = $dados['idusuario'];
-            $_SESSION["nome"] = $dados['nome'];
-
-            header("location:principal.php"); 
-           
-        }
-    } 
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -33,7 +7,7 @@ if(isset($_POST['logar'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
         integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <title>Login</title>
+    <title>CADASTRO</title>
 </head>
 
 <body style="background-image: url('fundo.png');">
@@ -41,15 +15,16 @@ if(isset($_POST['logar'])){
 
     <div class="container">
         <div class="text-center">         
-        <img src="foto2.png" class="rounded-circle" alt="Cinque Terre" width="190"> 
       </div>
 
-      <div class="jumbotron text-center mt-5" style="margin-bottom:0; background-color: #6fb695;">
+      <div class="jumbotron text-center mt-5" style="margin-bottom:0; background-color:#de7cba;">
 
     <div class="container">
     <div class="container p-3 my-3 border text-center">
-        <h1 class="text-light"> Login</h1>
-        <form method="POST" class="form-group">
+        <h1 class="text-light"> CADASTRE-SE</h1>
+                <h3 class="text-light">Para prosseguir sua viagem, é necessário cadastrar seu E-mail e sua senha!</h3>
+                <h5 class="text-light">Lembrando que o E-mail e senha do usuário é do responsável da crinça que será posteriormente cadastrada no sistema!</h5>
+                    <form method="POST" action="cadCria.php" class="form-group">
                 <div class="form-group">
                     <label  class="text-light"> E-mail </label>
                     <input class="form-control" type="text" name="email" required="required" />
@@ -60,7 +35,7 @@ if(isset($_POST['logar'])){
                     <input class="form-control" type="password" name="senha" required="required" />
                 </div>
 
-                <input class="btn btn-success" type="submit" role="button" name="logar" value="Concluir"/>
+                <input class="btn btn-light" type="submit" role="button" name="logar" value="Concluir"/>
 
               </form>
         </div>
